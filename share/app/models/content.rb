e156db04f6c54ac92abe76479fa8e6535776873f
arsenal <arsenal@webbie.bz>
2010-04-16 18:22:19 +0900

    waiwai

--- share/app/models/content.rb
+++ share/app/models/content.rb
@@ -1,3 +1,6 @@
 class Content < ActiveRecord::Base
   belongs_to :customer
+  validates_presence_of :title,:customer_id
+  validates_uniqueness_of :customer_id,:id
+  validates_numericality_of :id,:banner_size_id,:customer_id,:banner_id
 end
--- share/app/models/customer.rb
+++ share/app/models/customer.rb
@@ -1,3 +1,7 @@
 class Customer < ActiveRecord::Base
   has_many :contents
+  validates_presence_of :id,:address, :email, :person_in_charge
+  validates_uniqueness_of :id, :email
+  validates_numericality_of :id
+  validates_format_of :email, :with => /A([^@\s]+)@((?:[-a-z0-9]+\.)[a-z]{2,})\Z/i
 end
