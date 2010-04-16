135f95db2e39c8a5cf30e60a23ffb10546ce7fe8
Yu Usami <usami@realglobe.jp>
2010-04-16 16:41:39 +0900

    add content seeds

--- share/db/seeds.rb
+++ share/db/seeds.rb
@@ -5,3 +5,15 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+begin
+  require 'faker'
+rescue
+  puts "Please install faker with 'sudo gem install faker'"
+  exit 1
+end
+
+Content.delete_all
+10.times do
+  Content.create!(:title => Faker::Lorem.words, :body => Faker::Lorem.paragraph)
+end
