7e78a920a206e6db8915d33d8cbdb1602bacb6c6
Yu Usami <usami@realglobe.jp>
2010-04-22 02:46:24 +0900

    modify user model

--- share/db/schema.rb
+++ share/db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100319141813) do
+ActiveRecord::Schema.define(:version => 20100421164006) do
 
   create_table "admins", :force => true do |t|
     t.string   "name"
@@ -72,12 +72,17 @@ ActiveRecord::Schema.define(:version => 20100319141813) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "name"
-    t.string   "password_hash"
-    t.string   "password_salt"
-    t.string   "email"
+    t.string   "login",                     :limit => 40
+    t.string   "name",                      :limit => 100, :default => ""
+    t.string   "email",                     :limit => 100
+    t.string   "crypted_password",          :limit => 40
+    t.string   "salt",                      :limit => 40
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "remember_token",            :limit => 40
+    t.datetime "remember_token_expires_at"
   end
+
+  add_index "users", ["login"], :name => "index_users_on_login", :unique => true
 
 end
