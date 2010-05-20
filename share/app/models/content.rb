36b30f2760385d37e38a75ab78ffafeed8116db8
Yu Usami <usami@realglobe.jp>
2010-05-20 13:59:35 +0900

    modify content model

--- share/app/models/content.rb
+++ share/app/models/content.rb
@@ -7,22 +7,11 @@ class Content < ActiveRecord::Base
   has_many :tags, :through => :classifications
 
   validates_presence_of :title,:customer_id,:body ,:message =>"未記入の項目があります"
-  validates_length_of :title, :in => 5..100, :message =>"長さが不正です"
-  validates_length_of :body, :in => 5..1000, :message =>"長さが不正です"
-  validates_numericality_of :banner_size_id,:customer_id,:banner_id
+  validates_numericality_of :banner_size_id,:customer_id
 
   has_attached_file :banner, :styles => { :small => "150x150>" },
-                  :url  => "/assets/banner/:id/:style/:basename.:extension",
-  :path => ":rails_root/public/assets/banner/:id/:style/:basename.:extension"
-
-  validates_attachment_presence :banner ,:on => :update
-  validates_attachment_size :banner, :less_than => 5.megabytes, :on => :update
-  validates_attachment_content_type :banner, :content_type => ['image/jpeg', 'image/png'], :on => :update
-  validates_presence_of :customer_id
-  validates_presence_of :title,:body ,:message =>"未記入の項目があります", :on => :update
-  validates_length_of :title, :in => 5..100, :message =>"長さが不正です", :on => :update
-  validates_length_of :body, :in => 5..1000, :message =>"長さが不正です", :on => :update
-  validates_numericality_of :banner_size_id,:customer_id,:banner_id, :on => :update
+                    :url  => "/assets/banner/:id/:style/:basename.:extension",
+                    :path => ":rails_root/public/assets/banner/:id/:style/:basename.:extension"
 
   def content_image_attributes=(content_image_attributes)
     content_image_attributes.each do |attributes|
