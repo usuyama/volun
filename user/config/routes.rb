9ff797085b6849202beb651ac45dba7bd635baab
Yu Usami <usami@realglobe.jp>
2010-05-20 22:01:50 +0900

    import AuthenticatedSystem
    modify tag/index view for login
    
    change routes login_path to session/create

--- user/config/routes.rb
+++ user/config/routes.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
-  map.login '/login', :controller => 'sessions', :action => 'new'
+  map.login '/login', :controller => 'sessions', :action => 'create'
   map.register '/register', :controller => 'users', :action => 'create'
   map.signup '/signup', :controller => 'users', :action => 'new'
 
